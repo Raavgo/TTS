c4b43379ac15157154436229f60d22701d4a3814
Antonia Langer <ai21m032@technikum-wien.at>
2023-01-15 05:15:50 +0100

    Added Text-To-Speech Endpoint

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from speech_to_text import speech_to_text
+from text_to_speech import start_text_to_speech
 
 app = Flask(__name__)
 
@@ -16,6 +17,13 @@ def speech_to_text_route():
     result = speech_to_text(audio_file, language)
     #result = "Mocked result" + language + " " + audio_file.filename
     return to_json(result)
+
+@app.route('/text_to_speech', methods=['POST'])
+def text_to_speech_route():
+    language = str(request.form['lang'])
+    text = str(request.form['text'])
+    start_text_to_speech(text, language)
+    return 'ok'
 
 
 def to_json(string):
